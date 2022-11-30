74844e5e76481d642c82a0614aa22877f3652a70
mohamed hassan <hassenmohamed.92@gmail.com>
2022-11-30 11:55:30 +0100

    third commit

--- client/src/component/Message/Message.jsx
+++ client/src/component/Message/Message.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import "./Message.css";
 import profile from "../../images/profile.png";
+import ReactEmoji from "react-emoji";
 
 const Message = ({ user, message, classs }) => {
   if (user) {
     return (
       <div className={`message-box ${classs}`}>
-        <img src={profile} /> {`${user}:${message}`}
+        <img src={profile} alt="logo" /> {ReactEmoji.emojify(`${message}`)}
       </div>
     );
   } else {
     return (
       <div className={`message-box ${classs}`}>
-        <img src={profile} />
-        {`${message}`}
+        <img src={profile} alt="logo" />
+        {ReactEmoji.emojify(`${message}`)}
       </div>
     );
   }
